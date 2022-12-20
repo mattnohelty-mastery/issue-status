1de171289851e79f1c7691287450affb31d8cb1e
Alvin Land <113420615+alvin-land-mls@users.noreply.github.com>
2022-12-20 14:01:38 -0800

    subscribe buttons

--- src/components/subscribe/index.js
+++ src/components/subscribe/index.js
@@ -1,40 +1,112 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import styled from "styled-components";
+import toast, { Toaster } from "react-hot-toast";
 
 const FlexBox = styled.div`
   margin: 32px auto 0 auto;
   max-width: 1040px;
 `;
 
-const Label = styled.div`
+const Label = styled.label`
   margin-right: 8px;
   display: inline;
 `;
 
 const Button = styled.button`
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+  background: ${(props) => (props.primary ? "green" : "white")};
+  color: ${(props) => (props.primary ? "white" : "gray")};
 
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ${(props) => (props.primary ? "green" : "gray")};
   border-radius: 3px;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Input = styled.input`
   padding: 0.5em;
   margin: 0.5em;
-  color: ${props => props.inputColor || "palevioletred"};
-  background: ${props => props.inputColor || "white"};
-  border: none;
+  color: "black";
+  background: ${(props) => props.inputColor || "white"};
+  border: ${(props) => (props.isError ? "1px solid red" : "none")};
   border-radius: 3px;
 `;
 
-export default () =>
-    <FlexBox>
-        <Label>Subscribe to notifications:</Label>
-        <Input defaultValue="name@domain" type="text" />
-        <Button>Unsubscribe</Button>
-        <Button primary>Subscribe</Button>
-    </FlexBox>;
+export default () => {
+  const emailRegex = new RegExp(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  );
+  const notify = (type, msg) => {
+    switch (type) {
+      case "error":
+        toast.error(msg);
+        break;
+      case "info":
+      default:
+        toast(msg);
+        break;
+    }
+  };
+
+  const [email, setEmail] = useState("");
+  const [emailInputError, setEmailInputError] = useState(false);
+
+  const validateEmail = () => {
+    if (!emailRegex.test(email)) {
+      notify("error", "Please enter a valid email address");
+      setEmailInputError(true);
+      return false;
+    } else {
+      setEmailInputError(false);
+      return true;
+    }
+  };
+
+  const handleSubscribeClick = async (toSubscribeOrNotToSubscribe) => {
+    if (!validateEmail()) return;
+
+    const endpoint = toSubscribeOrNotToSubscribe ? "subscribe" : "unsubscribe";
+    try {
+      const response = await axios.get(`/${endpoint}?email=${email}`);
+      console.log(response);
+      notify("info", "Subscription Updated");
+    } catch (err) {
+      notify("error", "error :(");
+      console.error(err);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubscribeClick(true);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <FlexBox>
+        <Label htmlFor="emailInput">Subscribe to notifications:</Label>
+        <Input
+          placeholder="email@example.com"
+          type="email"
+          id="emailInput"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          isError={emailInputError}
+          onKeyDown={handleKeyDown}
+        />
+        <Button primary onClick={() => handleSubscribeClick(true)}>
+          Subscribe
+        </Button>
+        <Button onClick={() => handleSubscribeClick(false)}>Unsubscribe</Button>
+      </FlexBox>
+    </>
+  );
+};
